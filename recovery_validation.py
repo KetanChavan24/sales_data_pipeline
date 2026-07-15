5ba6871d981585635cc52c969a16f7478ab55d32
Ketan Chavan <ketanchavan248@gmail.com>
2026-07-15 11:32:02 +0530

    missing values validation

--- recovery_validation.py
+++ recovery_validation.py
@@ -9,6 +9,54 @@ mandat_columns = {
     "Region", "Country", "Order ID", "Order Date", "Ship Date", "Units Sold"
 }
 
+
+missing_values_cols = {}
+for col in mandat_columns:
+    individual_null_count_col = data[col].isnull().sum()
+    if(individual_null_count_col):
+        missing_values_cols.update({col: individual_null_count_col})
+        
+
+if(missing_values_cols):
+    print("========== DATA VALIDATION ==========")
+    print("Missing values found.")
+    for key,value in missing_values_cols.items():
+        print(f"{key}: {value}")
+else:
+    print("========== DATA VALIDATION ==========")
+    print("No missing values found.")
+    print("Validation Status : PASSED")
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #print(data.loc[3,"Region"])
 # null_count = 0
 # element = 0
@@ -17,26 +65,18 @@ mandat_columns = {
 #         if(data.loc[element,col] == 0):
 #             null_count += 1
 
-
-for col in mandat_columns:
-    individual_col_count = data[col].isnull().sum()
-    col_w_null = col
-    if(individual_col_count):
+# missing_value_columns = {}
+# for col in mandat_columns:
+#     individual_col_count = data[col].isnull().sum()
+#     col_w_null = col
+#     if(individual_col_count):
         
-        
+        # if(null_count == 0):
 
-
-
-
-
-if(null_count == 0):
-    print("========== DATA VALIDATION ==========")
-    print("No missing values found.")
-    print("Validation Status : PASSED")
-else:
-    print("========== DATA VALIDATION ==========")
-    print("Missing values:")
-    print("Validation Status : FAILED")
+# else:
+#     print("========== DATA VALIDATION ==========")
+#     print("Missing values:")
+#     print("Validation Status : FAILED")
 
 
 
--- trial.py
+++ trial.py
@@ -6,22 +6,8 @@ import sys
 
 file_path = "Europe_Sales_Records.csv"
 
-try :
-    data = pd.read_csv(file_path)
-except pd.errors.EmptyDataError:
-    print("The file is empty ")
-    sys.exit(1)
-except FileNotFoundError:
-    print("File not found")
-    sys.exit(1)
-except pd.errors.ParserError:
-    print("The file could not be parsed properly ")
-    sys.exit(1)
-except PermissionError:
-    print("Permission denied while reading this file ")
-    sys.exit(1)
-except Exception as e:
-    print(e)
-    sys.exit(1)
 
-print(data.columns.tolist())
+data = pd.read_csv(file_path)
+
+print(data["Sales Channel"].unique())
+print(data["Order Priority"].unique())
