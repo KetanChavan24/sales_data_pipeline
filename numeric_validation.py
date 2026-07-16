c573f4a73fc7012790631ab60d6c545fe761bbee
Ketan Chavan <ketanchavan248@gmail.com>
2026-07-16 17:49:06 +0530

    numeric validation cnfrm

--- numeric_validation.py
+++ numeric_validation.py
@@ -6,40 +6,31 @@ file_path = "Europe_Sales_Records.csv"
 data = pd.read_csv(file_path)
 
 numeric_rules = {
-    "pos" : ["unit price","Units Sold","Unit_cost"],
+    "pos" : ["Unit Price","Units Sold","Unit Cost"],
     "zero_pos" : ["Total Revenue","Total Cost"]
 }
 
 
-not_pos_col = 0
-not_pos_cols = {}
-not_zero_pos_col = 0
-not_zero_pos_cols = {}
+wrong_value_cols = {}
+wrong_value_count = 0
 
 for key in numeric_rules.keys():
     if(key == "pos"):
-        for col in numeric_rules.keys():
-            not_pos_col = data[col] <= 0
-            if(not_pos_col):
-                not_pos_cols.update({col : not_pos_col})
-
+        for col in numeric_rules[key]:
+            wrong_value_count  = (data[col] <= 0).sum()
+            if(wrong_value_count):
+                wrong_value_cols.update({col : wrong_value_count})
     elif(key == "zero_pos"):
-        for col in numeric_rules.keys():
-            not_zero_pos_col = data[col] < 0
-            if(not_zero_pos_col):
-                not_zero_pos_cols.update({col : not_zero_pos_col})
+        for col in numeric_rules[key]:
+            wrong_value_count  = (data[col] < 0).sum()
+            if(wrong_value_count ):
+                wrong_value_cols.update({col : wrong_value_count})
 
 
-if(not_pos_col):
+if(wrong_value_cols):
     print("========== NUMERIC VALIDATION ==========")
     print("Invalid Numeric Values")
-    for key,value in not_pos_cols.items():
-        print(f"{key} = {value}")
-    print("Validation Status : FAILED")    
-elif(not_zero_pos_cols):
-    print("========== NUMERIC VALIDATION ==========")
-    print("Invalid Numeric Values")
-    for key,value in not_zero_pos_cols.items():
+    for key,value in wrong_value_cols.items():
         print(f"{key} = {value}")
     print("Validation Status : FAILED")    
 else:
