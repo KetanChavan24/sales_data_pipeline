52c6cbed3c4d5a8d7a28152a26ab3c45d906ee0a
Ketan Chavan <ketanchavan@Ketans-MacBook-Air.local>
2026-07-20 23:25:38 +0530

    diss

--- cross_column(date)_validation.py
+++ cross_column(date)_validation.py
@@ -9,10 +9,27 @@ data = pd.read_csv(file_path)
 
 date_cols = ["Order Date" , "Ship Date"]
 str_to_date_data = pd.DataFrame()
+wrong_rows = {}
+wrong_row_count = 0
 
 for col in date_cols:
     str_to_date_data[col] = pd.to_datetime(data[col], errors="coerce")
+#print(str_to_date_data)
 
+# for val in str_to_date_data:
+
+# order_date = str_to_date_data["Order Date"]
+# ship_date = str_to_date_data["Ship Date"]
+
+# for odate in order_date:
+#     for sdate in ship_date:
+#         wrong_row_count = (data[])
+
+for row in str_to_date_data:
+    wrong_row_count = (data[data[]])
+
+
+    
 
 
 
