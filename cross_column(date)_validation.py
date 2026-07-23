63e9a0c9b1911f75f3a833db990fc8b0d8269b91
Ketan Chavan <ketanchavan248@gmail.com>
2026-07-23 12:35:49 +0530

    cross-column validation success!

--- cross_column(date)_validation.py
+++ cross_column(date)_validation.py
@@ -2,34 +2,35 @@ import pandas as pd
 
 file_path = "Europe_Sales_Records.csv"
 
-
 data = pd.read_csv(file_path)
 
 #CONSIDER THAT THE DATE VALUES ARE ALREADY VALIDATED AND HENCE WE ARE CONVERTING IT AGAIN IN DATETIME JUST FOR THE SAKE
 
 date_cols = ["Order Date" , "Ship Date"]
 str_to_date_data = pd.DataFrame()
-wrong_rows = {}
-wrong_row_count = 0
 
-for col in date_cols:
+
+for col in date_cols:   
     str_to_date_data[col] = pd.to_datetime(data[col], errors="coerce")
-#print(str_to_date_data)
 
-# for val in str_to_date_data:
+# for col in date_cols:  
+#     str_to_date_data[col] = pd.to_datetime(data[col] , errors="coerce") 
 
-# order_date = str_to_date_data["Order Date"]
-# ship_date = str_to_date_data["Ship Date"]
+invalid_rows = str_to_date_data[str_to_date_data["Ship Date"] < str_to_date_data["Order Date"]]
+wrong_row_count = (str_to_date_data["Ship Date"] < str_to_date_data["Order Date"]).sum()
 
-# for odate in order_date:
-#     for sdate in ship_date:
-#         wrong_row_count = (data[])
+if(wrong_row_count):
+    print("==========================================\n CROSS COLUMN VALIDATION \n ==========================================")
+    print("Rule : Ship date >= Order date")
+    print("Status : Failed")
+    print(f"Total Invalid Rows : {wrong_row_count}")
+    print(f"Rows affected : \n{invalid_rows}\n")
+else:
+    print("==========================================\n CROSS COLUMN VALIDATION \n ==========================================")
+    print("Rule : Ship date >= Order date")
+    print("Status : Success!")
+  
 
-for row in str_to_date_data:
-    wrong_row_count = (data[data[]])
 
 
-    
-
-
-
+# print(invalid_rows)
